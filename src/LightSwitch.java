de8136532d2de668bf99dec3f7c38a575c82b59e
Sandu <palii.sandu@gmail.com>
2021-11-22 10:25:30 +0200

    Question 1

--- src/LightSwitch.java
+++ src/LightSwitch.java
@@ -1,3 +1,13 @@
 public class LightSwitch {
+    private boolean lightsOn;
 
+    public LightSwitch() {
+        this.lightsOn = false;
+    }
+
+    public void printInfo(){
+        if (lightsOn){
+            System.out.println("Lights are on");
+        } else System.out.println("Lights are off");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        LightSwitch lightSwitch = new LightSwitch();
+        lightSwitch.printInfo();
     }
 }
