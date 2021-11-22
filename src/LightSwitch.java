cd2be8a343fb3c5f4b680c12b0a2b9721580a72d
Sandu <palii.sandu@gmail.com>
2021-11-22 13:26:30 +0200

    Question 2

--- src/LightSwitch.java
+++ src/LightSwitch.java
@@ -1,13 +1,30 @@
 public class LightSwitch {
-    private boolean lightsOn;
+    protected boolean lightsOn;
 
     public LightSwitch() {
-        this.lightsOn = false;
+        this.lightsOn = lightsOn;
     }
 
     public void printInfo(){
         if (lightsOn){
-            System.out.println("Lights are on");
-        } else System.out.println("Lights are off");
+            System.out.println("Lights are on.");
+        } else System.out.println("Lights are off.");
+    }
+}
+
+class Dimmer extends LightSwitch{
+    int brightness;
+
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+        lightsOn = brightness > 0;
+    }
+
+    @Override
+    public void printInfo() {
+        if (lightsOn)
+            System.out.println("Brightness is " + brightness + ".");
+        else
+            System.out.println("Lights are off.");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         LightSwitch lightSwitch = new LightSwitch();
         lightSwitch.printInfo();
+
+        Dimmer dimmer1 = new Dimmer();
+        dimmer1.setBrightness(80);
+        dimmer1.printInfo();
     }
 }
